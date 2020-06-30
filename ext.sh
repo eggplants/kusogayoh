4143a0b67d474e4d13062b08b69c4c2d54baa63e
eggplants <w10776e8w@yahoo.co.jp>
2020-06-30 21:57:03 +0900

    Add ext data

--- ext.sh
+++ ext.sh
@@ -1,1 +1,10 @@
-awk -F, 'NR>1{print$5}' kusogayoh_breakless.csv | grep '<br>' | tr -d \" > sentences
+#!/bin/bash
+
+awk -F, 'NR>1{print$5}' kusogayoh_breakless.csv |
+  grep '<br>'                                   |
+    tr -d \"                                    |
+      sed -r 's_(893413).*$_\1_g'               > sentences
+
+mecab -Owakati                                  <(
+  sed 's/<br>//g;s_https://shindanmaker.com/893413__g' sentences
+                                                )> sentences_wakati
---
+++ ext_eachValue.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+readonly FILE="sentences"
+readonly SAVE="data"
+
+function _extract(){
+  SAVEPATH="${SAVE}/${2}"
+  if [[ -f "${SAVEPATH}" ]]
+  then
+    echo -n 'overwhite ok?(y/n):' >&2
+    read -r f
+    [[ "${f}" -eq 'y' ]] && {
+      grep -oP "${1}" "${FILE}" | sed -r 's/[　 ]//g' > "${SAVEPATH}"
+    } || {
+      echo 'skipped.'
+    }
+  else
+    grep -oP "${1}" "${FILE}" | sed -r 's/[　 ]//g' > "${SAVEPATH}"
+  fi
+}
+
+function _chooser(){
+  case "${1}" in
+  title      ) _extract '^(?!が代).*?(が代)(?=<br><br>)'                    title      ;;
+  person     ) _extract '(?<=:)[^/]+?(?=<br>)'                              person     ;;
+  occupation ) _extract '(?<=<br>)[^(作詞)(作曲)(https):]+(?=:)'            occupation ;;
+  era1       ) _extract '(?<=<br><br>)[^(作詞)]*?(?=が代は )'               era1       ;;
+  era2       ) _extract '((?<=<br><br>)([^代]+)(?=代に)|^([^代]+)(?=代に))' era2       ;;
+  kuso       ) _extract '' kuso                                                        ;;
+  trans      ) _extract '(?<=<br><br>)[^<]+?(?=となりて<br><br>)'           trans      ;;
+  conqueror  ) _extract '' conqueror                                                   ;;
+  till       ) _extract '' till                                                        ;;
+  *          ) return 1                                                                ;;
+esac
+}
+
+function main(){
+  mkdir -p "${SAVE}"
+  local val
+  val=(title person occupation era1 era2 kuso trans conqueror till)
+  for v in "${val[@]}"
+  {
+    echo "Now: ${v}" >&2
+    _chooser "${v}"
+  }
+}
+
+main
+exit $?
